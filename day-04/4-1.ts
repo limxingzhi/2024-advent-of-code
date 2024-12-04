abfc2f1d2c2e733d55b3b0a9353c80260a403785
limxingzhi <limxz97@gmail.com>
2024-12-04 18:34:13 +0800

    day-04 : wip - getting further from the answer lol

--- day-04/4-1.ts
+++ day-04/4-1.ts
@@ -18,23 +18,23 @@ interface SearchNode extends Position {
 // get all of the surrounding items
 function getCandidates(x: number, y: number): Array<SearchNode> {
   return [
-    { val: lines?.[(x - 1)]?.[(y)], x: x - 1, y: y },
-    { val: lines?.[(x)]?.[(y - 1)], x: x, y: y - 1 },
-    { val: lines?.[(x + 1)]?.[(y)], x: x + 1, y: y },
-    { val: lines?.[(x)]?.[(y + 1)], x: x, y: y + 1 },
+    { val: lines?.[x - 1]?.[y], x: x - 1, y: y },
+    { val: lines?.[x]?.[y - 1], x: x, y: y - 1 },
+    { val: lines?.[x + 1]?.[y], x: x + 1, y: y },
+    { val: lines?.[x]?.[y + 1], x: x, y: y + 1 },
 
-    { val: lines?.[(x - 1)]?.[(y - 1)], x: x - 1, y: y - 1 },
-    { val: lines?.[(x + 1)]?.[(y - 1)], x: x + 1, y: y - 1 },
-    { val: lines?.[(x - 1)]?.[(y + 1)], x: x - 1, y: y + 1 },
-    { val: lines?.[(x + 1)]?.[(y + 1)], x: x + 1, y: y + 1 },
+    { val: lines?.[x - 1]?.[y - 1], x: x - 1, y: y - 1 },
+    { val: lines?.[x + 1]?.[y - 1], x: x + 1, y: y - 1 },
+    { val: lines?.[x - 1]?.[y + 1], x: x - 1, y: y + 1 },
+    { val: lines?.[x + 1]?.[y + 1], x: x + 1, y: y + 1 },
   ].filter(({ val }) => Boolean(val)) as Array<SearchNode>;
 }
 
 // add every single char into search queue
-const searchStack: Array<SearchNode> = [];
+const searchStack: Array<Array<SearchNode>> = [];
 lines.map((line, x) =>
   line.map((val, y) => {
-    if (val === searchStr[0]) searchStack.push({ x, y, val });
+    if (val === searchStr[0]) searchStack.push([{ x, y, val }]);
   }),
 );
 
@@ -45,25 +45,36 @@ const searchedSet = new Set<string>();
 
 let count = 0;
 
-while (searchStack.length > 0) {
-  const candidate = searchStack.pop() as SearchNode;
-  const searchChar = searchStr[candidate.val.length];
-
-  searchedSet.add(`${candidate.x}:${candidate.y}:${candidate.val}`);
-
-  // filter the neighbors to see if they have the char we want
-  const newCandidates = getCandidates(candidate.x, candidate.y)
-    .filter((item) => item.val === searchChar)
-    .filter((item) => !searchedSet.has(`${item.x}:${item.y}:${candidate.val}`));
-
-  searchStack.push(
-    ...newCandidates.map(({ val, ...rest }) => ({
-      ...rest,
-      val: candidate.val + val,
-    })),
-  );
-
-  if (candidate.val.length === 3) count += newCandidates.length;
+function serializedNodes(node: Array<SearchNode>): string {
+  return node
+    .map((candidate) => `${candidate.x}:${candidate.y}:${candidate.val}`)
+    .join("=");
 }
 
-console.log(Array.from(searchedSet).filter(item => item.includes('XMAS')).sort());
+while (searchStack.length > 0) {
+  const candidate = searchStack.pop() as Array<SearchNode>;
+  const searchChar = searchStr[candidate.length];
+
+  searchedSet.add(serializedNodes(candidate));
+
+  const lastNode = candidate.at(-1) as SearchNode;
+
+  // filter the neighbors to see if they have the char we want
+  const newCandidates = getCandidates(lastNode.x, lastNode.y).filter(
+    (item) => item.val === searchChar,
+  );
+  // .filter((item) => !searchedSet.has(serializedNodes(item)));
+
+  searchStack.push(
+    ...newCandidates.map((newCandy) => [...candidate, newCandy]),
+  );
+
+  if (candidate.length === 3) count += newCandidates.length;
+}
+
+console.log(
+  // count,
+  Array.from(searchedSet)
+    .filter((item) => item.includes("S"))
+    .sort(),
+);
